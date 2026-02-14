cb08d17e2d6cd9e9967ef41e3e375b02ef7b8f52
Sandeep Samuel <maybach9841@gmail.com>
2026-02-14 21:56:23 +0530

    improved server.py

--- src/server.py
+++ src/server.py
@@ -1,17 +1,26 @@
 import flwr as fl
 
+
+def weighted_average(metrics):
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+    
+   
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
 if __name__ == "__main__":
     strategy = fl.server.strategy.FedAvg(
-        fraction_fit=1.0,        # train on 100% of connected clients
-        fraction_evaluate=1.0,   # test on 100% of connected clients
-        min_fit_clients=2,       # wait for at least 2 clients to train
-        min_evaluate_clients=2,  # wait for at least 2 clients to test
-        min_available_clients=2, # wait for at least 2 clients to start
+        fraction_fit=1.0,
+        fraction_evaluate=1.0,
+        min_fit_clients=2,
+        min_evaluate_clients=2,
+        min_available_clients=2,
+        evaluate_metrics_aggregation_fn=weighted_average, 
     )
 
     print("Server starting... waiting for 2 clients to connect.")
     fl.server.start_server(
         server_address="0.0.0.0:8080", 
-        config=fl.server.ServerConfig(num_rounds=3),
+        config=fl.server.ServerConfig(num_rounds=10), 
         strategy=strategy,
     )
